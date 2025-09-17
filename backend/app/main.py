04486b94cfa12b8bd0c3d6880aae4b214afe9168
Addyk-24 <addykatkar24@gmail.com>
2025-09-17 19:51:27 +0530

    Working Database Manager

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,18 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
 from database.db_manager import Database_Manager
 
 db_manager = Database_Manager()
 
 class UserReport(BaseModel):
+    name: str
     location: str
     text_description : str
-    file_name : str
+    media_urls : str
+    hazard_type: Optional[str]
 
 
 app = FastAPI()
